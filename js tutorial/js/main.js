ccf7351f9afede4100589b1fa6be88885bb89391
Derryck <oseiagyemanb@gmail.com>
2022-12-14 22:31:46 +0100

    initial commit of file

---
+++ js tutorial/js/main.js
@@ -0,0 +1,12 @@
+var daughter = "Darina";
+daughter = "Hillary";
+
+let color = "blue";
+color = "red";
+
+const age = 10;
+age = 23;
+
+console.log(daughter);
+console.log(color);
+console.log(age);
