3bf36cfd05a019b8962b92c4a5ac4c1ee2c05626
Derryck <oseiagyemanb@gmail.com>
2022-12-15 12:51:46 +0100

    initial commit of files

---
+++ js tutorial/js/01-variables.js
@@ -0,0 +1,20 @@
+/*
+## Variables #1
+
+1. create "firstName" and "last_name" variables
+2. assign your values
+3. create "address" variable and assign "main street"
+   value to it
+4. re-assign address to "first street" later
+5. log all values in the console
+*/
+
+const firstName = "Derryck";
+const last_name = "Breeder";
+
+let address = "main street";
+address = "first's street";
+
+console.log(firstName);
+console.log(last_name);
+console.log(address);
---
+++ js tutorial/js/02-concat.js
@@ -0,0 +1,15 @@
+/* ## Concatenate Strings #2
+
+1. create "street" and "country" variables
+2. assign your values
+3. create "fullMailingAddress" variable and assign
+   the result of "street + country"
+4. remember about the space
+5. log "fullMailingAddress" in the console 
+*/
+
+let street = "cantoment";
+let country = "Ghana";
+let fullMailingAddress = street + " " + country;
+
+console.log(fullMailingAddress);
