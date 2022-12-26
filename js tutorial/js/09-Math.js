189c6836077984018f5378b968f67fbbe983ff94
Derryck <oseiagyemanb@gmail.com>
2022-12-26 18:06:38 +0100

    initial commit of files

---
+++ js tutorial/js/09-Math.js
@@ -0,0 +1,22 @@
+// const number = 3.32937;
+// const result = Math.floor(number)
+
+// const number = 3.32937;
+// const result = Math.ceil(number)
+
+// const number = 25;
+// const result = Math.sqrt(number)
+
+// const number = Math.PI;
+// const number = Math.min(4,5,7,6,2.2,8,2.01);
+// const number = Math.max(4,5,7,6,2.2,8,2.01);
+// const number = Math.PI;
+// const number = Math.random();
+
+/*
+Math.random() falls between 0 - 1, if we want bigger numbers we have mutiply it by that number range,
+let say we want a range of 20
+*/
+
+const number = Math.random() * 20;
+console.log(number)
