8f297e0d439c0c6557a0d0b51cbbd72296ad9abb
Derryck <oseiagyemanb@gmail.com>
2022-12-28 22:14:09 +0100

    initial commit

---
+++ js tutorial/js/statement.js
@@ -0,0 +1,43 @@
+// Switch statement
+// dice values : 1 - 6
+
+const dice = 10;
+
+switch (dice) {
+    case 1:
+        console.log('you got one')
+        break;
+    case 2:
+        console.log('you got two')
+        break;
+    case 3:
+        console.log('you got three')
+        break;
+    default:
+        console.log('you did not roll the dice')
+        break;
+}
+
+
+// else if statement
+// if (dice === 1) {
+//     console.log('you got one');
+// } else if (dice === 2) {
+//     console.log('you got two');
+// } else {
+//     console.log('you did not role the dice');
+// }
+
+
+
+// if statement
+// if (dice === 1) {
+//     console.log('you got one');
+// }
+// if (dice === 2) {
+//     console.log('you got two');
+// }
+// if (dice < 1 || dice > 6) {
+//     console.log('you did not role the dice');
+// }
+
