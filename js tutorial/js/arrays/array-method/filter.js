1456ad4c856b8b9f2e48913f13466ad36eda8d7d
Derryck <oseiagyemanb@gmail.com>
2023-02-23 12:39:00 +0100

    update of files

--- js tutorial/js/arrays/array-method/filter.js
+++ js tutorial/js/arrays/array-method/filter.js
@@ -1,83 +1,97 @@
-/*
-The filter() method is also mostly used, and the way it works is very simple. 
-It entails filtering out one or more items (a subset) from a larger collection of items (a superset) based on some condition/preference.
+// /*
+// The filter() method is also mostly used, and the way it works is very simple.
+// It entails filtering out one or more items (a subset) from a larger collection of items (a superset) based on some condition/preference.
 
-*/
+// */
 
-//Example 1: filtering avengers with age less than but equal to 35
-let avengers = [
-    {name: "steve rogers", age: 35, heroName: "captain america"},
-    {name: "tony stark", age: 42, heroName: "iron man"},
-    {name: "bruce banner", age: 40, heroName: "the hulk"},
-    {name: "peter parker", age: 26, heroName: "spiderman"},
-    {name: "tchalla", age: 32, heroName: "black panther"}
-]
+// //Example 1: filtering avengers with age less than but equal to 35
+// let avengers = [
+//     {name: "steve rogers", age: 35, heroName: "captain america"},
+//     {name: "tony stark", age: 42, heroName: "iron man"},
+//     {name: "bruce banner", age: 40, heroName: "the hulk"},
+//     {name: "peter parker", age: 26, heroName: "spiderman"},
+//     {name: "tchalla", age: 32, heroName: "black panther"}
+// ]
 
+// let youngActors = avengers.filter(avenger => avenger.age <= 35)
 
-let youngActors = avengers.filter(avenger => avenger.age <= 35)
+// console.log(youngActors)
 
-console.log(youngActors)
+// //Example 2: we filter out every person who is a toddler (whose age falls between 0 and 4 )
 
-//Example 2: we filter out every person who is a toddler (whose age falls between 0 and 4 )
+// let people = [
+//     {name: "aaron", age: 65},
+//     {name: "beth", age: 2},
+//     {name: "cara", age: 13},
+//     {name: "daniel", age: 3},
+//     {name: "ella", age: 25},
+//     {name: "fin", age: 1},
+//     {name: "george", age: 43},
+// ]
 
-let people = [
-    {name: "aaron", age: 65},
-    {name: "beth", age: 2},
-    {name: "cara", age: 13},
-    {name: "daniel", age: 3},
-    {name: "ella", age: 25},
-    {name: "fin", age: 1},
-    {name: "george", age: 43},
-]
+// let toddler = people.filter(tod => tod.age <= 4)
 
-let toddler = people.filter(tod => tod.age <= 4)
+// console.log(toddler)
 
-console.log(toddler)
+// //Example 3: In this example, we will only be filtering out the team members who are developers.
 
-//Example 3: In this example, we will only be filtering out the team members who are developers.
+// let team = [
+// 	{name: "aaron", position: "developer"},
+//     {name: "beth", position: "ui designer"},
+//     {name: "cara", position: "developer"},
+//     {name: "daniel", position: "content manager"},
+//     {name: "ella", position: "cto"},
+//     {name: "fin", position: "backend engineer"},
+//     {name: "george", position: "developer"},
+// ]
 
-let team = [
-	{name: "aaron", position: "developer"},
-    {name: "beth", position: "ui designer"},
-    {name: "cara", position: "developer"},
-    {name: "daniel", position: "content manager"},
-    {name: "ella", position: "cto"},
-    {name: "fin", position: "backend engineer"},
-    {name: "george", position: "developer"},
-]
+// let onlyDevelopers = team.filter(persons => persons.position === 'developer')
 
-let onlyDevelopers = team.filter(persons => persons.position === 'developer')
+// console.log(onlyDevelopers)
 
-console.log(onlyDevelopers)
+// //Example 4:
+// /*
+//     In this competition, there are three winners. The first will get the gold medal, the second will get silver, and the third, bronze.
+//     By using filter and accessing the index property of every item on each iteration, we can filter out each of the three winners into different variables.
+// */
 
+// let winners = ["Anna", "Beth", "Cara"];
 
-//Example 4: 
-/*
-    In this competition, there are three winners. The first will get the gold medal, the second will get silver, and the third, bronze.
-    By using filter and accessing the index property of every item on each iteration, we can filter out each of the three winners into different variables.
-*/
+// let gold = winners.filter((first, index) => index === 0)
+// let silver = winners.filter((second, index) => index === 1)
+// let bronze = winners.filter((third, index) => index === 2);
 
-let winners = ["Anna", "Beth", "Cara"];
+// console.log(`Gold winner: ${gold}, Silver winner: ${silver}, Bronze winner: ${bronze}`)
 
-let gold = winners.filter((first, index) => index === 0)
-let silver = winners.filter((second, index) => index === 1)
-let bronze = winners.filter((third, index) => index === 2);
+// //Example 5: How to use the array parameter
+// //In this example, we are going to define an array of four people. However, since there can only be three winners, the fourth person in the list will have to be discounted.
 
-console.log(`Gold winner: ${gold}, Silver winner: ${silver}, Bronze winner: ${bronze}`)
+// let competitors = ["Anna", "Beth", "Cara", "David"];
 
+// let displayWinners = (name, index, array) => {
+//     let isNextItem = index + 1 < array.length? true : false;
+//     if(isNextItem){
+//         console.log(`The winner ${index+1} is ${name}`)
+//     } else {
+//         console.log(`Sorry, ${name} finished ${index+1}`)
+//     }
+// }
 
-//Example 5: How to use the array parameter
-//In this example, we are going to define an array of four people. However, since there can only be three winners, the fourth person in the list will have to be discounted.
+// competitors.filter((name, index, array) => displayWinners(name, index, array))
 
-let competitors = ["Anna", "Beth", "Cara", "David"];
+// const scores = [2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14];
+// const firstOddNumber = scores.find((number) => number % 2 === 1);
+// console.log(firstOddNumber);
 
-let displayWinners = (name, index, array) => {
-    let isNextItem = index + 1 < array.length? true : false;
-    if(isNextItem){
-        console.log(`The winner ${index+1} is ${name}`)
-    } else {
-        console.log(`Sorry, ${name} finished ${index+1}`)
-    }
-}
+let employees = [
+   { name: "aaron", age: 35, position: "senior developer" },
+   { name: "beth", age: 25, position: "ui designer" },
+   { name: "cara", age: 19, position: "developer" },
+   { name: "daniel", age: 40, position: "content manager" },
+   { name: "ella", age: 27, position: "cto" },
+   { name: "fin", age: 30, position: "backend engineer" },
+   { name: "george", age: 22, position: "developer" },
+];
 
-competitors.filter((name, index, array) => displayWinners(name, index, array))
+const youngEmployees = employees.filter((person) => person.age < 30);
+console.log(youngEmployees);
--- js tutorial/js/arrays/array-method/reduce.js
+++ js tutorial/js/arrays/array-method/reduce.js
@@ -1,40 +1,61 @@
-/*
-The reduce() method iterates over an array and uses a callback function. It reduces the value in 
-the array into a single value. The single value can be a number, an array, object, etc but it will have one value.
+// /*
+// The reduce() method iterates over an array and uses a callback function. It reduces the value in
+// the array into a single value. The single value can be a number, an array, object, etc but it will have one value.
 
-This is pretty similar to the behavior of the Map function, but it has a twist–it allows us to return any value 
-from our function in a particular iteration, which will then exist as a parameter (argument) in that function in 
-the next iteration (that value is commonly known as the accumulator).
+// This is pretty similar to the behavior of the Map function, but it has a twist–it allows us to return any value
+// from our function in a particular iteration, which will then exist as a parameter (argument) in that function in
+// the next iteration (that value is commonly known as the accumulator).
 
-In the callback function, it uses two parameters:
-param 1 (acc -> accumlator): Total of all calculations
-param 2 (curr -> current value): current iteration/value 
-*/
+// In the callback function, it uses two parameters:
+// param 1 (acc -> accumlator): Total of all calculations
+// param 2 (curr -> current value): current iteration/value
+// */
 
-//Example 1: Calculating the sum of arrays
+// //Example 1: Calculating the sum of arrays
 
-const arrItems = [2,1,3,5,4]
+// const arrItems = [2,1,3,5,4]
 
-const sum = arrItems.reduce((acc, curr) => acc = acc + curr, 0)
+// const sum = arrItems.reduce((acc, curr) => acc = acc + curr, 0)
 
-console.log(sum)
+// console.log(sum)
 
-//Example 2 With Method2: Calculating total Salary 
-const staffsDetails = [
-    { name: "Jam Josh", age: 44, salary: 4000, currency: "USD" },
-    { name: "Justina Kap", age: 34, salary: 3000, currency: "USD" },
-    { name: "Chris Colt", age: 37, salary: 3700, currency: "USD" },
-    { name: "Jane Doe", age: 24, salary: 4200, currency: "USD" }
-];
+// //Example 2 With Method2: Calculating total Salary
+// const staffsDetails = [
+//     { name: "Jam Josh", age: 44, salary: 4000, currency: "USD" },
+//     { name: "Justina Kap", age: 34, salary: 3000, currency: "USD" },
+//     { name: "Chris Colt", age: 37, salary: 3700, currency: "USD" },
+//     { name: "Jane Doe", age: 24, salary: 4200, currency: "USD" }
+// ];
 
-function callbackFunction(acc, curr){
-    acc = acc + curr.salary;
-    console.log(acc, curr)
-    return acc;
-}
+// function callbackFunction(acc, curr){
+//     acc = acc + curr.salary;
+//     console.log(acc, curr)
+//     return acc;
+// }
 
-const initialValue = 0;
+// const initialValue = 0;
 
-const totalSalary = staffsDetails.reduce(callbackFunction, initialValue);
+// const totalSalary = staffsDetails.reduce(callbackFunction, initialValue);
 
-console.log(totalSalary)
+// console.log(totalSalary);
+
+// const animals = [
+//    "lion",
+//    "dog",
+//    "lion",
+//    "fish",
+//    "zebra",
+//    "lion",
+//    "dog",
+//    "zebra",
+// ];
+// const count = animals.reduce((acc, current) => {
+//    acc[current] = (acc[current] || 0) + 1;
+//    return acc;
+// }, {});
+// console.log(count);
+
+const numbers = [5, 10, 15, 20, 25];
+let sum = numbers.reduce((acc, curr) => acc + curr, 10);
+
+console.log(sum);
